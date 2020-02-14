ec48d5c53f365a479dc5cb903b359d54b0ee2ab3
s m hasan mansur <smhasanmansur@gmail.com>
2020-02-14 02:21:46 -0500

    done other than boundary case for window

--- hw2_cs682_smansur4/prob_1.py
+++ hw2_cs682_smansur4/prob_1.py
@@ -8,24 +8,32 @@ from tkinter import *
 
 def on_mouse_over(event, x, y, flags, param):
     global img
-    global gray
+    global dashboard
     if event == cv2.cv2.EVENT_MOUSEMOVE:
-        print("------------------")
         img_reset()
-        cv2.rectangle(img,(x-6, y-6), (x+6, y+6),(255,255,255),1)
-        print("p: x,y", x,y)
-        print("p: rgb", img[y][x][2], img[y][x][1], img[y][x][0])
+        cv2.rectangle(img,(x-6, y-6), (x+6, y+6),(0,0,255),1)
         intensity = sum(img[y][x])/3
-        print("p: intensity", intensity)
-        window = gray[y-5:y+5, x-5:x+5]
+        window = img[y-5:y+5, x-5:x+5]
         mean, std = cv2.meanStdDev(window)
-        print("window mean", mean)
-        print("window std", std)
-        cv2.putText(img, str(mean), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
+        str_coordinates = "x:{}, y:{}".format(x,y)
+        str_rgb = "R:{},G:{},B:{}".format(img[y][x][2], img[y][x][1], img[y][x][0])
+        str_intesity = "intensity:{}".format(sum(img[y][x])/3)
+        str_mean = "mean: R:{} G:{} B:{}".format(mean[2],mean[1], mean[0])
+        str_std = "standard deviation:" + "\n" + "R:{} G:{} B:{}".format(std[2],std[1], std[0])
+        output_str = str_coordinates + "\n" + str_rgb + "\n" + str_intesity + "\n" + str_mean + "\n" + str_std
+        y0, dy = 50, 50
+        for i, line in enumerate(output_str.split('\n')):
+            y = y0 + i*dy
+            cv2.putText(dashboard, str(line), (20, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
 
 def img_reset():
     global img
-    img = cv2.imread('testimage.png')
+    global dashboard
+    global filename
+    #img = cv2.imread("testimage.png")
+    img = cv2.imread(filename)
+    dashboard = np.full((400,900), 255, dtype='uint8')
+    cv2.imshow("dashboard", dashboard)
     cv2.imshow('image', img)
 
 def channel_histogram():
@@ -35,24 +43,31 @@ def channel_histogram():
         histr = cv2.calcHist([img], [i], None, [256], [0, 256])
         plt.plot(histr, color = col)
         plt.xlim([0, 256])
-    plt.show()
+    plt.show(block=False)
 
-#to handle image file with dialogbox
-#root = Tk()
-#root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("png files","*.png"),("all files","*.*")))
-#img = cv2.imread('testimage.png')
-#img = cv2.imread(root.filename)
 
-img = cv2.imread("testimage.png")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def main():
+    global img
+    global dashboard
+    global filename
 
-channel_histogram()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image",on_mouse_over)
+    #img = cv2.imread("testimage.png")
 
-while(1):
-    cv2.imshow("image", img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
-cv2.destroyAllWindows()
+    root = Tk()
+    root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("png files","*.png"),("all files","*.*")))
+    filename = root.filename
+    img = cv2.imread(filename)
+
+    dashboard = np.full((400,900), 255, dtype='uint8')
+    channel_histogram()
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image",on_mouse_over)
+    while(1):
+        cv2.imshow("image", img)
+        cv2.imshow("dashboard", dashboard)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
+
+main()
--- hw2_cs682_smansur4/prob_2.py
+++ hw2_cs682_smansur4/prob_2.py
@@ -3,17 +3,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 import glob
 
-def create_histograms(path):
-    files = [file for file in glob.glob(path + "*")]
-    files.sort()
+np.seterr(divide='ignore', invalid='ignore')
+
+def create_histograms(files):
     histograms = []
     for file in files:
             print(file)
-            img = cv2.imread(file)
-            img_uint16 = img.astype('uint16')
-            b_channels = img_uint16[:, :, 0]
-            g_channels = img_uint16[:, :, 1]
-            r_channels = img_uint16[:, :, 2]
+            img = cv2.imread(file).astype('uint16')
+            b_channels, g_channels, r_channels = img[:, :, 0], img[:, :, 1], img[:, :, 2]
             img_bit_shifted = ((b_channels >> 5)<<6) + ((g_channels>>5)<<3) + (r_channels>>5)
             img_flat = img_bit_shifted.ravel()
             histogram, bins = np.histogram(img_flat,bins=range(0,513))
@@ -25,10 +22,7 @@ def histogram_intersection(h1, h2):
     return intersection_val
 
 def histogram_chi2(h1, h2):
-    chi2_val = 0.0
-    for i in range(0,512):
-        if (h1[i]+h2[i]>0):
-            chi2_val += (h1[i]-h2[i])**2/(h1[i]+h2[i])
+    chi2_val = np.nansum(np.true_divide(np.square(np.subtract(h1, h2)), np.add(h1, h2)))
     return chi2_val
 
 def plot(plt, matrix, title):
@@ -36,7 +30,6 @@ def plot(plt, matrix, title):
     ax.set_xticks(np.arange(0, 99, 8))
     ax.set_yticks(np.arange(0, 99, 8))
     plt.setp(ax.get_xticklabels(), rotation=90, rotation_mode="anchor")
-
     ax.set_title(title)
     plt.imshow(matrix)
     plt.colorbar()
@@ -47,9 +40,13 @@ def main():
     hist_intersect_2 = []
     chi2_1 = []
     chi2_2 = []
+
+    files = [file for file in glob.glob("ST2MainHall4/" + "*")]
+    files.sort()
+
     print("computing histograms")
-    h = create_histograms('ST2MainHall4/')
-    print("computing histogram intersection & chi square value ...")
+    h = create_histograms(files)
+    print("computing histogram intersection & chi square measure ...")
     for i in range(0,99):
         for j in range(0,99):
             hist_intersect_2.append(histogram_intersection(h[i],h[j]))
@@ -58,22 +55,14 @@ def main():
         chi2_1.append(chi2_2)
         hist_intersect_2 = []
         chi2_2 = []
-
     hist_intersection = np.array(hist_intersect_1)
     chi_square = np.array(chi2_1)
 
-    #i_pos = [i for i in range(99)]
-    #j_pos = [j for j in range(99)]
+    g_max = 255
+    hist_intersection_scaled = g_max * hist_intersection
+    chi_square_scaled = g_max *(chi_square/max(chi_square.ravel()))
 
-    #fig, ax = plt.subplots(figsize=(500, 250))
+    plot(plt, hist_intersection_scaled, "Histogram intersection")
+    plot(plt, chi_square_scaled, "Chi-squared measure")
 
-    '''
-    ax.set_title("Image Pairs (Chi2)")
-    #plt.imshow(chi_square, cmap="Blues_r")
-    plt.imshow(chi_square)
-    plt.colorbar()
-    plt.show()
-    '''
-    plot(plt, hist_intersection, "hist intersection")
-    plot(plt, chi_square, "chi sqr")
 main()
